0c19fbddb191bdb2f6ab34719d38faf1304e211d
Theo Beers <tb3367@drexel.edu>
2024-09-23 09:21:21 -0400

    Specify DB URL

--- alembic/env.py
+++ alembic/env.py
@@ -1,8 +1,12 @@
+import os
 from logging.config import fileConfig
 
+from dotenv import load_dotenv
 from sqlalchemy import engine_from_config, pool
 
 from alembic import context
+
+load_dotenv()
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -37,7 +41,8 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    # url = config.get_main_option("sqlalchemy.url")
+    url = os.getenv("DATABASE_URL")
     context.configure(
         url=url,
         target_metadata=target_metadata,
