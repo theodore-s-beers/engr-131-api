232bf780bc0cc93f5387bebee7134e9fd0ca70d1
Theo Beers <tb3367@drexel.edu>
2024-09-24 15:06:33 -0400

    Add more endpoints, CRUD logic

---
+++ app/crud_admin.py
@@ -0,0 +1,23 @@
+from sqlalchemy.orm import Session
+
+from . import models, schemas
+
+
+def add_student(db: Session, student: schemas.Student):
+    db_student = models.Student(
+        email=student.email,
+        given_name=student.given_name,
+        family_name=student.family_name,
+    )
+    db.add(db_student)
+    db.commit()
+    db.refresh(db_student)
+    return db_student
+
+
+def get_student_by_email(db: Session, email: str):
+    return db.query(models.Student).filter(models.Student.email == email).first()
+
+
+def get_students(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Student).offset(skip).limit(limit).all()
--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 from typing import Annotated, TypeAlias
 
-from fastapi import Depends, FastAPI, HTTPException, Request, Response, status
+from fastapi import Depends, FastAPI, HTTPException, Request
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from sqlalchemy.orm import Session
 
+from . import crud_admin, schemas
 from .auth import verify_admin, verify_student
+from .db import SessionLocal
 
 app = FastAPI()
 
@@ -11,27 +14,42 @@ security = HTTPBasic()
 Credentials: TypeAlias = Annotated[HTTPBasicCredentials, Depends(security)]
 
 
+# DB dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+#
+# Student-accessible endpoints
+#
+
+
 @app.get("/")
 async def root():
-    return "Hello world"
+    return "Server is running"
 
 
 @app.post("/login")
 async def login(cr: Credentials):
     try:
         verify_admin(cr)  # Raises HTTPException on failure
-        return Response(status_code=status.HTTP_200_OK)
+        return "Admin credentials verified"
     except HTTPException:
         pass
 
     verify_student(cr)  # Raises HTTPException on failure
-    return Response(status_code=status.HTTP_200_OK)
+    return "Student credentials verified"
 
 
 @app.post("/live-scorer")
 async def live_scorer(req: Request, cr: Credentials):
     verify_student(cr)  # Raises HTTPException on failure
 
+    # This is just for debugging purposes
     body = await req.body()
     print(len(body.decode("utf-8")))
 
@@ -42,7 +60,38 @@ async def live_scorer(req: Request, cr: Credentials):
 async def upload_score(req: Request, cr: Credentials):
     verify_student(cr)  # Raises HTTPException on failure
 
+    # This is just for debugging purposes
     body = await req.body()
     print(len(body.decode("utf-8")))
 
     return "Score-upload request received"
+
+
+#
+# Admin-only endpoints
+#
+
+
+@app.get("/students", response_model=list[schemas.Student])
+async def get_students(
+    cr: Credentials,
+    skip: int = 0,
+    limit: int = 100,
+    db: Session = Depends(get_db),
+):
+    verify_admin(cr)
+    students = crud_admin.get_students(db, skip=skip, limit=limit)
+    return students
+
+
+@app.post("/students", response_model=schemas.Student)
+async def add_student(
+    cr: Credentials,
+    student: schemas.Student,
+    db: Session = Depends(get_db),
+):
+    verify_admin(cr)
+    db_student = crud_admin.get_student_by_email(db, email=student.email)
+    if db_student:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud_admin.add_student(db=db, student=student)
