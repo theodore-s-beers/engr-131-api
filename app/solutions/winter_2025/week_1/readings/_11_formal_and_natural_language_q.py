d80415c47e85be03a5325d47ef06cc78f02d86d1
Theo Beers <tb3367@drexel.edu>
2024-12-16 19:18:41 -0500

    Update solutions

--- app/solutions/winter_2025/week_1/readings/_11_formal_and_natural_language_q.py
+++ app/solutions/winter_2025/week_1/readings/_11_formal_and_natural_language_q.py
@@ -1,6 +1,6 @@
 from typing import Any
 
-total_points: float = 16.0
+total_points: float = 21.0
 
 solutions: dict[str, Any] = {
     "q1-1-Natural-v-Formal Languages": "Formal languages are designed to be unambiguous, while natural languages rely on context.",
@@ -9,5 +9,10 @@ solutions: dict[str, Any] = {
     "q1-4-Python-Print-Function": 'Displays the literal text `"The other shoe fell."` on the screen.',
     "q2-1-Programming-Syntax": "Structure",
     "q2-2-Parsing-in-Programming": "Breaking a sentence or statement into tokens and understanding its structure.",
+    "q4-1-True/False Questions": "False",
+    "q4-2-# **Parsing in natural languages happens subconsciously, while parsing in formal languages requires explicit effort.**": "True",
+    "q4-3-# **A missing comma in a Python program can cause a syntax error.**": "True",
+    "q4-4-# **Both natural and formal languages are equally tolerant of errors.**": "False",
+    "q4-5-# **Formal languages like Python are valuable for solving engineering problems systematically.**": "True",
     "q3-1-Features-of-Formal-Languages": ["Conciseness", "Strict syntax rules"],
 }
