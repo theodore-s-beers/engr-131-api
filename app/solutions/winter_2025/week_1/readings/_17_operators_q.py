d5ce9995f5122ba7f1a33a62e0d82682fb73ea2c
Theo Beers <tb3367@drexel.edu>
2024-12-16 19:15:00 -0500

    Update solutions

---
+++ app/solutions/winter_2025/week_1/readings/_17_operators_q.py
@@ -0,0 +1,12 @@
+from typing import Any
+
+total_points: float = 14.0
+
+solutions: dict[str, Any] = {
+    "q1-1-Python-Operators": "Perform operations on one or more operands",
+    "q1-2-Addition-Operator": "`+`",
+    "q3-1-multiplication-operator": "False",
+    "q3-2-Logical-Operators": "True",
+    "q3-1-Categories-of-Python-Operators": ["Logical", "Arithmetic", "Membership"],
+    "q3-2-Arithmetic-Operators": ["`%`", "`**`", "`+`"],
+}
