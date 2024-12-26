68e64f9606009f0f582ff15bb73a96069930dc46
Theo Beers <tb3367@drexel.edu>
2024-12-26 18:27:11 -0500

    Add key generation scripts

---
+++ generate_client_keypair.py
@@ -0,0 +1,35 @@
+import base64
+import os
+
+import nacl.public
+
+private_key_path = ".client_private_key.bin"
+public_key_path = ".client_public_key.bin"
+
+if os.path.exists(private_key_path) or os.path.exists(public_key_path):
+    print("A key already exists at one of the specified paths")
+
+    if os.path.exists(public_key_path):
+        with open(public_key_path, "rb") as f:
+            public_key = nacl.public.PublicKey(f.read())
+            key_b64 = base64.b64encode(public_key.encode())
+            key_str = key_b64.decode()
+            print("Client public key:", key_str)
+
+    exit()
+
+# Generate keypair
+private_key = nacl.public.PrivateKey.generate()
+public_key = private_key.public_key
+
+# Save keys to files
+with open(private_key_path, "wb") as f:
+    f.write(private_key.encode())
+with open(public_key_path, "wb") as f:
+    f.write(public_key.encode())
+
+print("Generated and saved client keypair")
+
+public_key_b64 = base64.b64encode(public_key.encode())
+public_key_str = public_key_b64.decode()
+print("Client public key:", public_key_str)
