c495bf7ed65de2c5c42ecb54a3bd27138386c4d0
jagar2 <jca92@drexel.edu>
2025-01-02 16:22:37 -0500

    update to make get solutions more restrictive

--- move_solutions.py
+++ move_solutions.py
@@ -32,9 +32,8 @@ SUBMODULE_PATH = Path("vendor") / "course-content"
 def get_solution_paths() -> list[Path]:
     paths = [
         path
-        for path in SUBMODULE_PATH.rglob("*.py")
-        if "blah" not in path.parts
-        and "_solutions" in path.parts
+        for path in SUBMODULE_PATH.rglob("*_q.py")
+        if "_solutions" in path.parts
         and "autograder" in path.parts
     ]
 
