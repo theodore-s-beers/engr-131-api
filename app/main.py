a4a09d17c453600dc0d29735f67c8ddd917772cb
jagar2 <jca92@drexel.edu>
2025-01-03 19:33:23 -0500

    update with convert to float.

--- app/main.py
+++ app/main.py
@@ -292,6 +292,8 @@ async def score_assignment(
     current_best = crud_student.get_best_score(
         db=db, student_email=student_email, assignment=assignment_title
     )
+    
+    current_best = float(current_best)
 
     if current_best is None or modified_grade > current_best:
         current_best = modified_grade
