12aaafec85a002c18cafec3ca70fefe49245ee4a
jagar2 <jca92@drexel.edu>
2025-01-05 21:54:29 -0500

    update files manually

--- app/solutions/winter_2025/week_1/readings/_15_variables_q.py
+++ app/solutions/winter_2025/week_1/readings/_15_variables_q.py
@@ -1,15 +1,11 @@
 from typing import Any
 
-total_points: list[float] = [2.0, 2.0, 1.0, 4.0, 4.0]
+total_points: float = [1.0, 1.0, 1.0, 2.0, 2.0]
 
 solutions: dict[str, Any] = {
-    "q1-1-Descriptive-Variable-Names": "To make the code easier to understand and maintain",
-    "q1-2-Converting-Variable-Types": "Using the `float()` function to convert a string to a numeric type",
-    "q3-1-Constants-tf": "False",
-    "q3-1-Rules-for-Naming-Variables": [
-        "Variables must start with a letter or an underscore (`_`).",
-        "Variable names are case-sensitive.",
-        "Variables can include numbers but cannot start with them.",
-    ],
-    "q3-2-Multi-Variable-Assignment": ["`a, b, c = 1, 2, 3`", "`a = b = c = 0`"],
+    "q1-1-Descriptive-Variable-Names": 'To make the code easier to understand and maintain',
+    "q1-2-Converting-Variable-Types": 'Using the `float()` function to convert a string to a numeric type',
+    "q3-1-Constants-tf": 'False',
+    "q3-1-Rules-for-Naming-Variables": ['Variables must start with a letter or an underscore (`_`).', 'Variable names are case-sensitive.', 'Variables can include numbers but cannot start with them.'],
+    "q3-2-Multi-Variable-Assignment": ['`a, b, c = 1, 2, 3`', '`a = b = c = 0`'],
 }
--- app/solutions/winter_2025/week_1/readings/_9_what_is_a_program_q.py
+++ app/solutions/winter_2025/week_1/readings/_9_what_is_a_program_q.py
@@ -1,15 +1,15 @@
 from typing import Any
 
-total_points: list[float] = [2.0, 2.0, 2.0, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+total_points: float = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
 
 solutions: dict[str, Any] = {
-    "q1-1-What-is-a-Program": "A sequence of instructions for a computer to execute.",
-    "q1-2-Program-Input": "Printing results to the screen.",
-    "q1-3-Purpose-of-Loops": "To repeat actions with slight variations.",
-    "q1-4-Art-of-the-Breakdown": "Breaking down large problems into smaller, manageable tasks.",
-    "q2-1-Program-recipe": "True",
-    "q2-2-Output-keyboard": "False",
-    "q2-3-Conditional-Execution": "True",
-    "q2-4-Repetition": "True",
-    "q2-5-Relevance of Programming": "False",
+    "q1-1-What-is-a-Program": 'A sequence of instructions for a computer to execute.',
+    "q1-2-Program-Input": 'Printing results to the screen.',
+    "q1-3-Purpose-of-Loops": 'To repeat actions with slight variations.',
+    "q1-4-Art-of-the-Breakdown": 'Breaking down large problems into smaller, manageable tasks.',
+    "q2-1-Program-recipe": 'True',
+    "q2-2-Output-keyboard": 'False',
+    "q2-3-Conditional-Execution": 'True',
+    "q2-4-Repetition": 'True',
+    "q2-5-Relevance of Programming": 'False',
 }
