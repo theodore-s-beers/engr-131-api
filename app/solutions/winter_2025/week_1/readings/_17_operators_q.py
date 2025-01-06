e2e7e5b184ae1d1ed011348fa7d5a5d2e34b8728
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2025-01-06 14:21:28 -0500

    Update solution modules

--- app/solutions/winter_2025/week_1/readings/_17_operators_q.py
+++ app/solutions/winter_2025/week_1/readings/_17_operators_q.py
@@ -3,10 +3,10 @@ from typing import Any
 total_points: float = [2.0, 2.0, 1.0, 1.0, 4.0, 4.0]
 
 solutions: dict[str, Any] = {
-    "q1-1-Python-Operators": 'Perform operations on one or more operands',
-    "q1-2-Addition-Operator": '`+`',
-    "q3-1-multiplication-operator": 'False',
-    "q3-2-Logical-Operators": 'True',
-    "q2-1-Categories-of-Python-Operators": ['Logical', 'Arithmetic', 'Membership'],
-    "q2-2-Arithmetic-Operators": ['`%`', '`**`', '`+`'],
+    "q1-1-Python-Operators": "Perform operations on one or more operands",
+    "q1-2-Addition-Operator": "`+`",
+    "q3-1-multiplication-operator": "False",
+    "q3-2-Logical-Operators": "True",
+    "q2-1-Categories-of-Python-Operators": ["Logical", "Arithmetic", "Membership"],
+    "q2-2-Arithmetic-Operators": ["`%`", "`**`", "`+`"],
 }
