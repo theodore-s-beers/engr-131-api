42d50a50ca26afa92603e9bb522a74c11d79a95f
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2025-01-06 22:04:22 -0500

    Organize functions

--- app/crud_admin.py
+++ app/crud_admin.py
@@ -201,32 +201,6 @@ def add_assignment(db: Session, assignment: schemas.Assignment) -> models.Assign
     return db_assignment
 
 
-def add_notebook(db: Session, notebook: schemas.Notebook) -> models.Notebook:
-    """
-    Add a new notebook to the database.
-
-    Args:
-        db (Session): The database session to use for the operation.
-        notebook (schemas.Notebook): The notebook data to be added.
-
-    Returns:
-        models.Notebook: The newly created notebook object.
-    """
-    db_notebook = models.Notebook(
-        title=notebook.title,
-        week_number=notebook.week_number,
-        assignment_type=notebook.assignment_type,
-        max_score=notebook.max_score,
-        due_date=notebook.due_date,
-    )
-
-    db.add(db_notebook)
-    db.commit()
-    db.refresh(db_notebook)
-
-    return db_notebook
-
-
 def get_assignment_by_title(db: Session, title: str) -> Optional[models.Assignment]:
     """
     Retrieve an assignment from the database by its title.
@@ -256,6 +230,73 @@ def get_assignments(db: Session) -> Sequence[models.Assignment]:
     return db.execute(stmt).scalars().all()
 
 
+def update_assignment(
+    db: Session, title: str, assignment: schemas.Assignment
+) -> models.Assignment:
+    """
+    Add a new assignment to the database.
+
+    Args:
+        db (Session): The database session to use for the operation.
+        assignment (schemas.Assignment): The assignment data to be added.
+
+    Returns:
+        models.Assignment: The newly created assignment object.
+    """
+
+    stmt = select(models.Assignment).where(models.Assignment.title == title)
+    db_assignment = db.execute(stmt).scalar_one_or_none()
+
+    if not db_assignment:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Assignment not found",
+        )
+
+    db_assignment.title = assignment.title
+    db_assignment.description = assignment.description
+    db_assignment.max_score = assignment.max_score
+    db_assignment.due_date = assignment.due_date
+    db_assignment.week_number = assignment.week_number
+    db_assignment.assignment_type = assignment.assignment_type
+
+    db.commit()
+    db.refresh(db_assignment)
+
+    return db_assignment
+
+
+#
+# Notebooks table
+#
+
+
+def add_notebook(db: Session, notebook: schemas.Notebook) -> models.Notebook:
+    """
+    Add a new notebook to the database.
+
+    Args:
+        db (Session): The database session to use for the operation.
+        notebook (schemas.Notebook): The notebook data to be added.
+
+    Returns:
+        models.Notebook: The newly created notebook object.
+    """
+    db_notebook = models.Notebook(
+        title=notebook.title,
+        week_number=notebook.week_number,
+        assignment_type=notebook.assignment_type,
+        max_score=notebook.max_score,
+        due_date=notebook.due_date,
+    )
+
+    db.add(db_notebook)
+    db.commit()
+    db.refresh(db_notebook)
+
+    return db_notebook
+
+
 def get_notebook_by_title(db: Session, title: str) -> Optional[models.Notebook]:
     """
     Retrieve a notebook from the database by its title.
@@ -283,6 +324,40 @@ def get_notebooks(db: Session) -> Sequence[models.Notebook]:
     """
     stmt = select(models.Notebook)
     return db.execute(stmt).scalars().all()
+
+
+def update_notebook(
+    db: Session, title: str, notebook: schemas.Notebook
+) -> models.Notebook:
+    """
+    Adds a new notebook to the database.
+
+    Args:
+        db (Session): The database session to use for the operation.
+        notebook (schemas.Notebook): The notebook data to be added
+
+    Returns:
+        models.Notebook: The newly created notebook object.
+    """
+    stmt = select(models.Notebook).where(models.Notebook.title == title)
+    db_notebook = db.execute(stmt).scalar_one_or_none()
+
+    if not db_notebook:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Notebook not found",
+        )
+
+    db_notebook.title = notebook.title
+    db_notebook.week_number = notebook.week_number
+    db_notebook.assignment_type = notebook.assignment_type
+    db_notebook.max_score = notebook.max_score
+    db_notebook.due_date = notebook.due_date
+
+    db.commit()
+    db.refresh(db_notebook)
+
+    return db_notebook
 
 
 #
@@ -346,6 +421,48 @@ def create_token(db: Session, token_req: schemas.TokenRequest) -> models.Token:
     return db_token
 
 
+def get_token_by_value(db: Session, value: str) -> Optional[models.Token]:
+    """
+    Retrieve a token from the database by its value.
+
+    Args:
+        db (Session): The database session to use for the query.
+        value (str): The value of the token to retrieve.
+
+    Returns:
+        Optional[models.Token]: The token object if found, otherwise None.
+    """
+    stmt = select(models.Token).where(models.Token.value == value)
+    return db.execute(stmt).scalar_one_or_none()
+
+
+# TODO: Add a way to track history of all tokens
+def update_token(db: Session, token: schemas.TokenRequest):
+    stmt = select(models.Token).where(models.Token.value == token.value)
+    db_token = db.execute(stmt).scalar_one_or_none()
+
+    if not db_token:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Token not found",
+        )
+
+    db_token.value = token.value
+    db_token.created = datetime.now()
+    db_token.expires = db_token.created + timedelta(minutes=token.duration)
+    db_token.requester = token.requester
+
+    db.commit()
+    db.refresh(db_token)
+
+    return db_token
+
+
+#
+# Grades table
+#
+
+
 def get_assignment_grades(
     db: Session,
     week_number: int,
@@ -381,110 +498,3 @@ def get_assignment_grades(
         {"student_email": row.student_email, "best_score": row.best_score}
         for row in result
     ]
-
-
-def get_token_by_value(db: Session, value: str) -> Optional[models.Token]:
-    """
-    Retrieve a token from the database by its value.
-
-    Args:
-        db (Session): The database session to use for the query.
-        value (str): The value of the token to retrieve.
-
-    Returns:
-        Optional[models.Token]: The token object if found, otherwise None.
-    """
-    stmt = select(models.Token).where(models.Token.value == value)
-    return db.execute(stmt).scalar_one_or_none()
-
-
-def update_assignment(
-    db: Session, title: str, assignment: schemas.Assignment
-) -> models.Assignment:
-    """
-    Add a new assignment to the database.
-
-    Args:
-        db (Session): The database session to use for the operation.
-        assignment (schemas.Assignment): The assignment data to be added.
-
-    Returns:
-        models.Assignment: The newly created assignment object.
-    """
-
-    stmt = select(models.Assignment).where(models.Assignment.title == title)
-    db_assignment = db.execute(stmt).scalar_one_or_none()
-
-    if not db_assignment:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Assignment not found",
-        )
-
-    db_assignment.title = assignment.title
-    db_assignment.description = assignment.description
-    db_assignment.max_score = assignment.max_score
-    db_assignment.due_date = assignment.due_date
-    db_assignment.week_number = assignment.week_number
-    db_assignment.assignment_type = assignment.assignment_type
-
-    db.commit()
-    db.refresh(db_assignment)
-
-    return db_assignment
-
-
-# TODO: Add a way to track history of all tokens
-def update_token(db: Session, token: schemas.TokenRequest):
-    stmt = select(models.Token).where(models.Token.value == token.value)
-    db_token = db.execute(stmt).scalar_one_or_none()
-
-    if not db_token:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Token not found",
-        )
-
-    db_token.value = token.value
-    db_token.created = datetime.now()
-    db_token.expires = db_token.created + timedelta(minutes=token.duration)
-    db_token.requester = token.requester
-
-    db.commit()
-    db.refresh(db_token)
-
-    return db_token
-
-
-def update_notebook(
-    db: Session, title: str, notebook: schemas.Notebook
-) -> models.Notebook:
-    """
-    Adds a new notebook to the database.
-
-    Args:
-        db (Session): The database session to use for the operation.
-        notebook (schemas.Notebook): The notebook data to be added
-
-    Returns:
-        models.Notebook: The newly created notebook object.
-    """
-    stmt = select(models.Notebook).where(models.Notebook.title == title)
-    db_notebook = db.execute(stmt).scalar_one_or_none()
-
-    if not db_notebook:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Notebook not found",
-        )
-
-    db_notebook.title = notebook.title
-    db_notebook.week_number = notebook.week_number
-    db_notebook.assignment_type = notebook.assignment_type
-    db_notebook.max_score = notebook.max_score
-    db_notebook.due_date = notebook.due_date
-
-    db.commit()
-    db.refresh(db_notebook)
-
-    return db_notebook
