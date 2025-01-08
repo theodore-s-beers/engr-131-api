0395fc0a7f72e707b07bd49bb1a374ebbed29ed7
jagar2 <jca92@drexel.edu>
2025-01-08 17:31:28 -0500

    update week2 questions

--- app/solutions/winter_2025/week_2/readings/_12_precision_q.py
+++ app/solutions/winter_2025/week_2/readings/_12_precision_q.py
@@ -1,13 +1,9 @@
 from typing import Any
 
-total_points: list[float] = [1.0, 1.0, 2.0]
+total_points: float = [1.0, 1.0, 2.0]
 
 solutions: dict[str, Any] = {
-    "q1-1-Floating-Point-Precision": "abs((0.1 + 0.2) - 0.3) < 1e-10",
-    "q3-1-Precision-Loss-tf": "True",
-    "q2-1-Number-Representation": [
-        "Integers in Python have unlimited precision",
-        "Floating-point numbers can represent decimal numbers",
-        "The math.isclose() function is better than == for comparing floats",
-    ],
+    "q1-1-Floating-Point-Precision": 'abs((0.1 + 0.2) - 0.3) < 1e-10',
+    "q3-1-Precision-Loss-tf": 'True',
+    "q2-1-Number-Representation": ['Integers in Python have unlimited precision', 'Floating-point numbers can represent decimal numbers', 'The math.isclose() function is better than == for comparing floats'],
 }
