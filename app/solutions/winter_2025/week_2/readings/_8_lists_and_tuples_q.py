fea3afe1900513eeb543b58fd7779d0a1ec5ec8f
jagar2 <jca92@drexel.edu>
2025-01-08 13:54:34 -0500

    update solutions

--- app/solutions/winter_2025/week_2/readings/_8_lists_and_tuples_q.py
+++ app/solutions/winter_2025/week_2/readings/_8_lists_and_tuples_q.py
@@ -1,19 +1,11 @@
 from typing import Any
 
-total_points: list[float] = [2.0, 2.0, 1.0, 4.0, 4.0]
+total_points: float = [1.0, 1.0, 1.0, 2.0, 2.0]
 
 solutions: dict[str, Any] = {
-    "q1-1-Immutable-Properties": "Because atomic properties like atomic number and mass are immutable physical constants",
-    "q1-2-List-Usage-Materials": "Recording a sequence of heat treatment steps for an alloy",
-    "q3-1-Mutability-Understanding": "False",
-    "q2-1-Mutable-Properties": [
-        "Processing temperatures for heat treatment",
-        "Processing times for each manufacturing step",
-        "Selected alloying elements for a new material design",
-    ],
-    "q2-2-Immutable-Constants": [
-        "Crystal structure of pure elements at standard conditions",
-        "Atomic mass of isotopes",
-        "Melting points of pure elements",
-    ],
+    "q1-1-Immutable-Properties": 'Because atomic properties like atomic number and mass are immutable physical constants',
+    "q1-2-List-Usage-Materials": 'Recording a sequence of heat treatment steps for an alloy',
+    "q3-1-Mutability-Understanding": 'False',
+    "q2-1-Mutable-Properties": ['Processing temperatures for heat treatment', 'Processing times for each manufacturing step', 'Selected alloying elements for a new material design'],
+    "q2-2-Immutable-Constants": ['Crystal structure of pure elements at standard conditions', 'Atomic mass of isotopes', 'Melting points of pure elements'],
 }
