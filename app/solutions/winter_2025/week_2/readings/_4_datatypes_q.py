2f603c2291274222a0da0d2cb544b390c4133972
CI/CD Runner <theo.beers@drexel.edu>
2025-01-10 11:28:27 -0500

    Update submodule and move solution files

--- app/solutions/winter_2025/week_2/readings/_4_datatypes_q.py
+++ app/solutions/winter_2025/week_2/readings/_4_datatypes_q.py
@@ -1,12 +1,18 @@
 from typing import Any
 
-total_points: float = [1.0, 1.0, 1.0, 1.0, 2.0, 2.0]
+total_points: list[float] = [1.0, 1.0, 1.0, 1.0, 2.0, 2.0]
 
 solutions: dict[str, Any] = {
-    "q1-1-Atomic-Number-Storage": 'Integer',
-    "q1-2-Atomic-Mass-Storage": 'Float',
-    "q3-1-Dictionary-Element-Properties": 'True',
-    "q3-2-Proton-Storage": 'False',
-    "q2-1-Electron-Configuration-Storage": ['String', 'List', 'Tuple'],
-    "q2-2-Element-Properties-Storage": ['dictionary', 'list', 'tuple', 'float', 'string'],
+    "q1-1-Atomic-Number-Storage": "Integer",
+    "q1-2-Atomic-Mass-Storage": "Float",
+    "q3-1-Dictionary-Element-Properties": "True",
+    "q3-2-Proton-Storage": "False",
+    "q2-1-Electron-Configuration-Storage": ["String", "List", "Tuple"],
+    "q2-2-Element-Properties-Storage": [
+        "dictionary",
+        "list",
+        "tuple",
+        "float",
+        "string",
+    ],
 }
