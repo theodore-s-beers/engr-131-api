e3db38033c174e21364d7bd477e6e7a5afc9a014
CI/CD Runner <theo.beers@drexel.edu>
2025-01-10 13:28:39 -0500

    Update submodule and move solution files

--- app/solutions/winter_2025/week_2/practicequiz/_1_practice_quiz_data_types_q.py
+++ app/solutions/winter_2025/week_2/practicequiz/_1_practice_quiz_data_types_q.py
@@ -1,12 +1,15 @@
 from typing import Any
 
-total_points: float = [2.0, 2.0, 2.0, 2.0, 4.0, 4.0]
+total_points: list[float] = [2.0, 2.0, 2.0, 2.0, 4.0, 4.0]
 
 solutions: dict[str, Any] = {
-    "q1-1-Data-Structure-Modification": 'Dictionary',
-    "q1-2-Type-Conversion": '5.14 (Float)',
-    "q3-1-String-Operations": 'False',
-    "q3-2-List-Uniqueness": 'False',
-    "q2-1-Dictionary-Operations": ['Adding new key-value pairs after creation', 'Using a integer as a dictionary key'],
-    "q2-2-List-Methods": ['`append()`', '`sort()`'],
+    "q1-1-Data-Structure-Modification": "Dictionary",
+    "q1-2-Type-Conversion": "5.14 (Float)",
+    "q3-1-String-Operations": "False",
+    "q3-2-List-Uniqueness": "False",
+    "q2-1-Dictionary-Operations": [
+        "Adding new key-value pairs after creation",
+        "Using a integer as a dictionary key",
+    ],
+    "q2-2-List-Methods": ["`append()`", "`sort()`"],
 }
