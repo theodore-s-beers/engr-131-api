56883c4b9afd259122a3494444e0dc067e6fe923
jagar2 <jca92@drexel.edu>
2025-01-10 11:39:46 -0500

    add more solutions

--- app/solutions/winter_2025/week_1/readings/_7_what_is_python_q.py
+++ app/solutions/winter_2025/week_1/readings/_7_what_is_python_q.py
@@ -1,9 +1,9 @@
 from typing import Any
 
-total_points: list[float] = [1.0, 1.0, 1.0]
+total_points: float = [1.0, 1.0, 1.0]
 
 solutions: dict[str, Any] = {
-    "q1-1-High-Level-v-Low-Level": "High-level languages are closer to human language and must be translated for computers to execute.",
-    "q1-2-Python-Advantages": "Python is a low-level language, so it runs directly on hardware without translation.",
-    "q1-3-Python-Interpreter": "It translates and executes Python code step by step.",
+    "q1-1-High-Level-v-Low-Level": 'High-level languages are closer to human language and must be translated for computers to execute.',
+    "q1-2-Python-Advantages": 'Python is a low-level language, so it runs directly on hardware without translation.',
+    "q1-3-Python-Interpreter": 'It translates and executes Python code step by step.',
 }
