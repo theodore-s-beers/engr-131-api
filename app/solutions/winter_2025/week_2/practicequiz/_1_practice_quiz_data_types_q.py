b635adbc90c0c1385b8465743319949614eded17
jagar2 <jca92@drexel.edu>
2025-01-10 20:17:15 -0500

    update key-value pair

--- app/solutions/winter_2025/week_2/practicequiz/_1_practice_quiz_data_types_q.py
+++ app/solutions/winter_2025/week_2/practicequiz/_1_practice_quiz_data_types_q.py
@@ -7,6 +7,6 @@ solutions: dict[str, Any] = {
     "q1-2-Type-Conversion": '5.14 (Float)',
     "q3-1-String-Operations": 'False',
     "q3-2-List-Uniqueness": 'False',
-    "q2-1-Dictionary-Operations": ['Adding new key-value pairs after creation', 'Using a integer as a dictionary key'],
+    "q2-1-Dictionary-Operations": ['Adding new key value pairs after creation', 'Using a integer as a dictionary key'],
     "q2-2-List-Methods": ['append()', 'sort()'],
 }
