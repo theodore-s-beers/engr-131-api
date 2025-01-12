08e36318ca46cec2dc0c46368556a5c3a50be459
jagar2 <jca92@drexel.edu>
2025-01-11 21:45:32 -0500

    update with week 2 quiz

---
+++ app/solutions/winter_2025/week_2/quiz/quiz_2_q.py
@@ -0,0 +1,12 @@
+from typing import Any
+
+total_points: float = [2.0, 2.0, 2.0, 2.0, 4.0, 4.0]
+
+solutions: dict[str, Any] = {
+    "q1-1-List-Immutability": 'List',
+    "q1-2-String-Operations": 'Hello5',
+    "q3-1-List-Slicing": 'False',
+    "q3-2-Dictionary-Keys": 'True',
+    "q2-1-Dictionary-Operations": [],
+    "q2-2-**String Basics**": [],
+}
