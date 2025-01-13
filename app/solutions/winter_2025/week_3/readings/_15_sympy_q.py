f9f916452affe7645c2ca105b2d0eafbc340a4a6
jagar2 <jca92@drexel.edu>
2025-01-13 18:18:58 -0500

    move week 3 solutions

---
+++ app/solutions/winter_2025/week_3/readings/_15_sympy_q.py
@@ -0,0 +1,15 @@
+from typing import Any
+
+total_points: float = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0]
+
+solutions: dict[str, Any] = {
+    "q1-1-what-calculates-derivatives": '`diff()`',
+    "q1-2-what-calculates-integrals": '`integrate()`',
+    "q1-3-what-calculates-limits": 'The behavior of a function as it approaches a value',
+    "q3-1-linsolve-non-linear-equations": 'False',
+    "q3-2-sympy-solves-nonlinear-equations": 'True',
+    "q3-3-dsolve-function": 'True',
+    "q3-4-dsolve-function-numpy": 'False',
+    "q2-1-what-is-the-diff-function-used-for-in-sympy": ['Calculate the rate of change of a function', 'Find the slope of a curve', 'Calculate higher-order derivatives'],
+    "q2-2-what-is-the-integrate-function-used-for-in-sympy": ['Compute the area under a curve', 'Calculate the volume of a solid'],
+}
---
+++ app/solutions/winter_2025/week_3/readings/_9_numpy_q.py
@@ -0,0 +1,14 @@
+from typing import Any
+
+total_points: float = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0]
+
+solutions: dict[str, Any] = {
+    "q1-1-Why-is-NumPy-faster-than-Python-lists-for-numerical-computations": 'NumPy uses efficient C-based implementation for arrays',
+    "q1-2-characteristics-of-NumPy-arrays": 'Arrays have a fixed size after creation',
+    "q1-3-shape-of-a-NumPy-array": '`.shape`',
+    "q3-1-Numpy-basics-mutibility": 'True',
+    "q3-2-NumPy-basics-broadcasting": 'True',
+    "q3-3-NumPy-basics-sort": 'False',
+    "q2-1-preallocating-memory-in-numpy": ['`np.zeros()`', '`np.ones()`', '`np.empty()`'],
+    "q2-2-element-wise-operations-in-numpy": ['Addition', 'Subtraction', 'Multiplication'],
+}
