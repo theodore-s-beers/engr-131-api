fcdfc1fd0ceaf4fc01394c784f36f4541d70a057
CI/CD Runner <theo.beers@drexel.edu>
2025-01-13 19:32:34 -0500

    Update submodule and move solution files

--- app/solutions/winter_2025/week_3/readings/_7_python_tools_q.py
+++ app/solutions/winter_2025/week_3/readings/_7_python_tools_q.py
@@ -1,14 +1,18 @@
 from typing import Any
 
-total_points: float = [2.0, 2.0, 2.0, 1.0, 1.0, 1.0, 4.0, 4.0]
+total_points: list[float] = [2.0, 2.0, 2.0, 1.0, 1.0, 1.0, 4.0, 4.0]
 
 solutions: dict[str, Any] = {
-    "q1-1-what-is-numpy-used-for": 'Numerical operations on large arrays and matrices',
-    "q1-2-which-is-pandas-most-suitable-for": 'Handling and analyzing structured data',
-    "q1-3-what-is-matplotlib-used-for": 'Creating visualizations such as plots and graphs',
-    "q3-1-sympy-and-viz": 'False',
-    "q3-2-ml-libraries": 'True',
-    "q3-3-opencv-TF": 'False',
-    "q2-1-match-libraries-to-purposes": ['Pandas: Handling structured data', 'SciPy: Advanced scientific computing', 'Matplotlib: Creating visualizations'],
-    "q2-2-numpy-or-sympy": ['NumPy', 'SymPy'],
+    "q1-1-what-is-numpy-used-for": "Numerical operations on large arrays and matrices",
+    "q1-2-which-is-pandas-most-suitable-for": "Handling and analyzing structured data",
+    "q1-3-what-is-matplotlib-used-for": "Creating visualizations such as plots and graphs",
+    "q3-1-sympy-and-viz": "False",
+    "q3-2-ml-libraries": "True",
+    "q3-3-opencv-TF": "False",
+    "q2-1-match-libraries-to-purposes": [
+        "Pandas: Handling structured data",
+        "SciPy: Advanced scientific computing",
+        "Matplotlib: Creating visualizations",
+    ],
+    "q2-2-numpy-or-sympy": ["NumPy", "SymPy"],
 }
