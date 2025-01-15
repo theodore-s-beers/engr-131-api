b70d3b4b6db8267d4319626b331e117c5e9f4b0c
jagar2 <jca92@drexel.edu>
2025-01-15 14:37:31 -0500

    update build

--- app/solutions/winter_2025/week_3/readings/_2_errors_q.py
+++ app/solutions/winter_2025/week_3/readings/_2_errors_q.py
@@ -1,19 +1,13 @@
 from typing import Any
 
-total_points: list[float] = [1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0]
+total_points: float = [1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0]
 
 solutions: dict[str, Any] = {
-    "q1-1-syntax-errors": "The program doesn’t run at all",
-    "q1-2-semantic-errors": "Your program runs but produces incorrect results",
-    "q3-1-Syntax-Errors-TF": "False",
-    "q3-2-Semantic-Errors-TF": "False",
-    "q3-3-Error-Debugging-TF": "True",
-    "q2-1-Runtime-Error-Characteristics": [
-        "They occur during program execution",
-        "They can be caused by dividing by zero",
-    ],
-    "q2-2-Error-Handling-Techniques": [
-        "Using `try/except` blocks",
-        "Validating user inputs before performing operations",
-    ],
+    "q1-1-syntax-errors": 'The program doesn’t run at all',
+    "q1-2-semantic-errors": 'Your program runs but produces incorrect results',
+    "q3-1-Syntax-Errors-TF": 'False',
+    "q3-2-Semantic-Errors-TF": 'False',
+    "q3-3-Error-Debugging-TF": 'True',
+    "q2-1-Runtime-Error-Characteristics": ['They occur during program execution', 'They can be caused by dividing by zero'],
+    "q2-2-Error-Handling-Techniques": ['Using `try/except` blocks', 'Validating user inputs before performing operations'],
 }
