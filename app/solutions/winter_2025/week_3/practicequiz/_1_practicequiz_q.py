fd0d8ba99fb35bb51fb4b6fcdc18fa3fecb8508d
jagar2 <jca92@drexel.edu>
2025-01-20 14:26:16 -0500

    update move

--- app/solutions/winter_2025/week_3/practicequiz/_1_practicequiz_q.py
+++ app/solutions/winter_2025/week_3/practicequiz/_1_practicequiz_q.py
@@ -6,6 +6,6 @@ solutions: dict[str, Any] = {
     "q1-1-Sport-Car-Engine-Tracking-System": 'np.max()',
     "q1-2-Default-Fuel-Type-in-Car-Array": 'It uses C code under the hood',
     "q1-3-NumPy Basics in Engine Performance Analysis": 'Creates a 5x5 array filled with zeros.',
-    "q2-1-SymPy-Capabilities-in-Performance-Tuning": ['Solving equations symbolically', 'Calculating definite integrals', 'Performing symbolic differentiation.'],
+    "q2-1-SymPy-Capabilities-in-Performance-Tuning": ['Solving equations symbolically.', 'Calculating definite integrals.', 'Performing symbolic differentiation.'],
     "q2-2-Valid-NumPy-Array-Creation-Methods": ['np.zeros()', 'np.array([])', 'np.arange()', 'np.random.random()'],
 }
--- app/solutions/winter_2025/week_3/quiz/_1_quiz_q.py
+++ app/solutions/winter_2025/week_3/quiz/_1_quiz_q.py
@@ -6,6 +6,6 @@ solutions: dict[str, Any] = {
     "q1-1-Shape-of-the-Bread-Tracking-Array": '.shape',
     "q1-2-Default-Data-Type-in-Smart-Toaster-Arrays": 'int',
     "q1-3-NumPy Basics in Smart Toasters": 'Creates a 3x3 array filled with zeros.',
-    "q2-1-SymPy-Capabilities-in-Toaster-Mechanics": ['Solving equations symbolically', 'Calculating definite integrals', 'Performing symbolic differentiation.'],
+    "q2-1-SymPy-Capabilities-in-Toaster-Mechanics": ['Solving equations symbolically.', 'Calculating definite integrals.', 'Performing symbolic differentiation.'],
     "q2-2-Valid-NumPy-Array-Creation-Methods": ['np.zeros()', 'np.array([])', 'np.arange()', 'np.random.random()'],
 }
