9e975e0e4139b1e72d4e9eb8f62fc6d77af941ed
jagar2 <jca92@drexel.edu>
2025-01-27 13:49:14 -0500

    update quiz responses

--- app/solutions/winter_2025/week_4/quiz/_1_quiz_q.py
+++ app/solutions/winter_2025/week_4/quiz/_1_quiz_q.py
@@ -5,7 +5,7 @@ total_points: float = [1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0]
 solutions: dict[str, Any] = {
     "q1-1-pressure-monitoring-system": 'High Pressure Warning',
     "q1-2-loop-monitoring-sensors": 'The loop runs indefinitely.',
-    "q3-1-sensor-failure-conditions": 'True',
+    "q3-1-sensor-failure-conditions": 'False',
     "q3-2-manufacturing-loop": 'True',
     "q3-3-temperature-control-break": 'True',
     "q2-1-valve-control-if-else": ['An `else` block is optional.', 'You can use multiple `elif` conditions to handle different flow rate ranges.', 'The `if` statement evaluates conditions to determine which code block to execute.'],
