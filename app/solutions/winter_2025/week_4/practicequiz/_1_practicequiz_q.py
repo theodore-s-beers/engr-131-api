73e18a497210ae17bde4d8b617200d4dd0afec1e
Theo Beers <theo.beers@drexel.edu>
2025-01-28 18:31:00 +0000

    Update submodule and move solution files

--- app/solutions/winter_2025/week_4/practicequiz/_1_practicequiz_q.py
+++ app/solutions/winter_2025/week_4/practicequiz/_1_practicequiz_q.py
@@ -4,11 +4,11 @@ total_points: list[float] = [1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0]
 
 solutions: dict[str, Any] = {
     "q1-1-solar-panel-monitoring": "Optimal Solar Output",
-    "q1-2-# **You’re analyzing wind turbine efficiency data. What happens if a `while` loop is used to monitor wind speeds but lacks a `break` statement in its logic?**": "The loop runs indefinitely.",
-    "q3-1-# **An air quality monitoring system can use an `if` statement to evaluate conditions, such as whether particulate matter exceeds a safe threshold.**": "True",
+    "q1-2-wind-turbine-efficiency": "The loop runs indefinitely.",
+    "q3-1-air-quality-check": "True",
     "q3-2-wind-speed-loop": "True",
     "q3-3-solar-battery-break": "True",
-    "q2-1-# **You’re implementing logic for managing battery charging based on voltage levels. Which of the following statements about Python’s `if-else` are true?**": [
+    "q2-1-battery-charge-logic": [
         "An `else` block is optional.",
         "You can use multiple `elif` conditions to handle different voltage ranges.",
         "The `if` statement evaluates conditions to determine which code block to execute.",
