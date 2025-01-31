9f70934e655a7670299d616aca661ca120585833
Theo Beers <theo.beers@drexel.edu>
2025-01-31 22:27:01 +0000

    Update submodule and move solution files

--- app/solutions/winter_2025/week_4/readings/_3_southpark_q.py
+++ app/solutions/winter_2025/week_4/readings/_3_southpark_q.py
@@ -1,13 +1,13 @@
 from typing import Any
 
-total_points: float = [1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0]
+total_points: list[float] = [1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0]
 
 solutions: dict[str, Any] = {
-    "q1-1-southpark-boolean-evaluation": 'True',
-    "q1-2-boolean-conversion-scenarios": 'An empty list `[]`',
-    "q3-1-logical-negation": 'True',
-    "q3-2-short-circuiting": 'True',
-    "q3-3-equality-checks": 'False',
-    "q2-1-if-else-logical-operators": ['`and`', '`or`', '`not`'],
-    "q2-2-southpark-if-else": ['When `kenny_alive` is `False`'],
+    "q1-1-southpark-boolean-evaluation": "True",
+    "q1-2-boolean-conversion-scenarios": "An empty list `[]`",
+    "q3-1-logical-negation": "True",
+    "q3-2-short-circuiting": "True",
+    "q3-3-equality-checks": "False",
+    "q2-1-if-else-logical-operators": ["`and`", "`or`", "`not`"],
+    "q2-2-southpark-if-else": ["When `kenny_alive` is `False`"],
 }
