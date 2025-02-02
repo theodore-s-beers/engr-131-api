cfe89dd751acf50ba474ed968e290b436467701f
jagar2 <jca92@drexel.edu>
2025-02-02 12:39:14 -0500

    update questions

--- app/solutions/winter_2025/week_5/practicequiz/_1_practicequiz_q.py
+++ app/solutions/winter_2025/week_5/practicequiz/_1_practicequiz_q.py
@@ -1,10 +1,13 @@
 from typing import Any
 
-total_points: float = [1.0, 1.0, 2.0, 2.0]
+total_points: float = [1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0]
 
 solutions: dict[str, Any] = {
     "q1-1-function-power-calculation": 'A function must be declared with the `def` keyword.',
     "q1-2-args-kwargs-smartgrid": 'Both `*args` and `**kwargs`',
+    "q3-1-function-scope": 'False',
+    "q3-2-args-order": 'True',
+    "q3-3-optional-arguments": 'True',
     "q2-1-return-multiple-values": ['A function can return multiple values using tuples.', 'A function can return different data types at the same time.'],
     "q2-2-kwargs-usage": ['`**kwargs` allows passing multiple keyword arguments into a function.', '`**kwargs` must always be the last argument in the function signature.', '`**kwargs` arguments are accessed as a dictionary inside the function.'],
 }
