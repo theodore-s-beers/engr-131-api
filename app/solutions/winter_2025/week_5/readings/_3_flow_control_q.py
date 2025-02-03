49430975d2a356779a58904cc82e092b86956205
jagar2 <jca92@drexel.edu>
2025-02-02 22:01:26 -0500

    update question

--- app/solutions/winter_2025/week_5/readings/_3_flow_control_q.py
+++ app/solutions/winter_2025/week_5/readings/_3_flow_control_q.py
@@ -1,20 +1,13 @@
 from typing import Any
 
-total_points: list[float] = [1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0]
+total_points: float = [1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0]
 
 solutions: dict[str, Any] = {
-    "q1-1-mall-parking-calculator": "10",
-    "q1-2-narrow-path-conditions": "Access blocked!",
-    "q3-1-house-accessibility": "True",
-    "q3-2-shop-opening-loops": "True",
-    "q3-3-escalator-shutdown": "True",
-    "q2-1-shop-opening-functions": [
-        "A function can take the opening time and closing time as arguments.",
-        "A function can calculate the total hours a shop is open.",
-        "A function can return multiple outputs, such as the opening and closing times.",
-    ],
-    "q2-2-mall-escalator-loops": [
-        "A `for` loop can iterate over a schedule of hours.",
-        "A `while` loop can run as long as the escalators are operational.",
-    ],
+    "q1-1-mall-parking-calculator": '10',
+    "q1-2-narrow-path-conditions": 'Access blocked!',
+    "q3-1-house-accessibility": 'True',
+    "q3-2-shop-opening-loops": 'True',
+    "q3-3-escalator-shutdown": 'True',
+    "q2-1-shop-opening-functions": ['A function can take the opening time and closing time as arguments.', 'A function can calculate the total hours a shop is open.', 'A function can return multiple outputs, such as the opening and closing times.'],
+    "q2-2-mall-escalator-loops": ['A `for` loop can iterate over a schedule of hours.', 'A `while` loop can run as long as the escalators are operational.'],
 }
