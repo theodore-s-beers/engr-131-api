211a73706f1230ccb7d149aa4a18ae64eef3206f
jagar2 <jca92@drexel.edu>
2025-02-04 17:03:51 -0500

    update solutions

--- app/solutions/winter_2025/week_2/readings/_10_dictionaries_q.py
+++ app/solutions/winter_2025/week_2/readings/_10_dictionaries_q.py
@@ -1,10 +1,10 @@
 from typing import Any
 
-total_points: list[float] = [1.0, 1.0, 1.0, 2.0]
+total_points: float = [1.0, 1.0, 1.0, 2.0]
 
 solutions: dict[str, Any] = {
-    "q1-1-Dictionary-Access": "100",
+    "q1-1-Dictionary-Access": '100',
     "q1-2-Dictionary-Modification": '`metrics["shares"] = 25`',
-    "q3-1-Dictionary-Values": "True",
-    "q2-1-Valid-Dictionary-Keys": ['`"pageviews"`', "`42`"],
+    "q3-1-Dictionary-Values": 'True',
+    "q2-1-Valid-Dictionary-Keys": ['`"pageviews"`', '`42`'],
 }
