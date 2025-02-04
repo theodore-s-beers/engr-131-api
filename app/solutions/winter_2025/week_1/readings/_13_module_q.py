0d7ea4d57877fa81391fd93703f4632543ac55eb
Theo Beers <theo.beers@drexel.edu>
2025-02-04 22:27:10 +0000

    Update submodule and move solution files

--- app/solutions/winter_2025/week_1/readings/_13_module_q.py
+++ app/solutions/winter_2025/week_1/readings/_13_module_q.py
@@ -1,10 +1,14 @@
 from typing import Any
 
-total_points: float = [1.0, 1.0, 2.0, 2.0]
+total_points: list[float] = [1.0, 1.0, 2.0, 2.0]
 
 solutions: dict[str, Any] = {
-    "q1-1-What-is-a-Python-Module": 'A Python file containing reusable code.',
-    "q2-1-Python-built-in-modules": 'False',
-    "q3-1-Examples-of-Python-Built-in-Modules": ['`os`', '`sys`', '`math`'],
-    "q3-2-Statements-about-Python-Modules": ['Modules promote code reuse.', 'You can import a specific function from a module.', 'Python packages are a collection of modules.'],
+    "q1-1-What-is-a-Python-Module": "A Python file containing reusable code.",
+    "q2-1-Python-built-in-modules": "False",
+    "q3-1-Examples-of-Python-Built-in-Modules": ["`os`", "`sys`", "`math`"],
+    "q3-2-Statements-about-Python-Modules": [
+        "Modules promote code reuse.",
+        "You can import a specific function from a module.",
+        "Python packages are a collection of modules.",
+    ],
 }
--- app/solutions/winter_2025/week_5/readings/_8_arguments_and_return_q.py
+++ app/solutions/winter_2025/week_5/readings/_8_arguments_and_return_q.py
@@ -1,13 +1,20 @@
 from typing import Any
 
-total_points: float = [1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0]
+total_points: list[float] = [1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0]
 
 solutions: dict[str, Any] = {
-    "q1-1-door-direction-parameters": 'Wrong',
-    "q1-2-door-return-values": 'It returns `None` by default.',
-    "q3-1-default-parameters": 'True',
-    "q3-2-multiple-arguments": 'False',
-    "q3-3-return-early": 'True',
-    "q2-1-function-parameters": ['Parameters are placeholders for arguments passed to a function.', 'A function can accept multiple parameters.'],
-    "q2-2-return-statement-usage": ["The `return` statement ends a function's execution.", 'A function can return multiple values.', 'If no `return` statement is provided, the function returns `None`.'],
+    "q1-1-door-direction-parameters": "Wrong",
+    "q1-2-door-return-values": "It returns `None` by default.",
+    "q3-1-default-parameters": "True",
+    "q3-2-multiple-arguments": "False",
+    "q3-3-return-early": "True",
+    "q2-1-function-parameters": [
+        "Parameters are placeholders for arguments passed to a function.",
+        "A function can accept multiple parameters.",
+    ],
+    "q2-2-return-statement-usage": [
+        "The `return` statement ends a function's execution.",
+        "A function can return multiple values.",
+        "If no `return` statement is provided, the function returns `None`.",
+    ],
 }
