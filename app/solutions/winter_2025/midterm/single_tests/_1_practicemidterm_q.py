c101ef3a158853bf2c356a0499e40b448c92a953
jagar2 <jca92@drexel.edu>
2025-02-04 19:08:18 -0500

    update with new practice

---
+++ app/solutions/winter_2025/midterm/single_tests/_1_practicemidterm_q.py
@@ -0,0 +1,8 @@
+from typing import Any
+
+total_points: float = [2.0, 2.0]
+
+solutions: dict[str, Any] = {
+    "q2-1-python-lists": ['Lists are mutable.', 'Lists can contain elements of different data types.'],
+    "q2-2-python-dictionaries": ['Dictionaries store key-value pairs.', 'Dictionary keys must be unique.', 'Dictionaries are ordered in modern Python versions.'],
+}
