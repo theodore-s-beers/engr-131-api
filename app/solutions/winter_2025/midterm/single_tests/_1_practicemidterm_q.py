a59ec43474957725969b23e2c2b859b7eb4fb860
Theo Beers <theo.beers@drexel.edu>
2025-02-05 00:35:00 +0000

    Update submodule and move solution files

--- app/solutions/winter_2025/midterm/single_tests/_1_practicemidterm_q.py
+++ app/solutions/winter_2025/midterm/single_tests/_1_practicemidterm_q.py
@@ -1,8 +1,15 @@
 from typing import Any
 
-total_points: float = [2.0, 2.0]
+total_points: list[float] = [2.0, 2.0]
 
 solutions: dict[str, Any] = {
-    "q2-1-python-lists": ['Lists are mutable.', 'Lists can contain elements of different data types.'],
-    "q2-2-python-dictionaries": ['Dictionaries store key-value pairs.', 'Dictionary keys must be unique.', 'Dictionaries are ordered in modern Python versions.'],
+    "q2-1-python-lists": [
+        "Lists are mutable.",
+        "Lists can contain elements of different data types.",
+    ],
+    "q2-2-python-dictionaries": [
+        "Dictionaries store key-value pairs.",
+        "Dictionary keys must be unique.",
+        "Dictionaries are ordered in modern Python versions.",
+    ],
 }
