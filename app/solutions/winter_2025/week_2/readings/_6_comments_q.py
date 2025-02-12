9ba6fbbc49138bc8e8ba540026d189130451dfe3
jagar2 <jca92@drexel.edu>
2025-02-12 10:32:14 -0500

    update with additional question responses

--- app/solutions/winter_2025/week_2/readings/_6_comments_q.py
+++ app/solutions/winter_2025/week_2/readings/_6_comments_q.py
@@ -1,16 +1,12 @@
 from typing import Any
 
-total_points: list[float] = [1.0, 1.0, 1.0, 1.0, 2.0, 2.0]
+total_points: float = [1.0, 1.0, 1.0, 1.0, 2.0, 2.0]
 
 solutions: dict[str, Any] = {
-    "q2-1-description-of-a-comment-in-python": "Text ignored by the Python interpreter, used to explain code",
-    "q2-2-Comment-Functionality": "Explains the purpose of the program",
-    "q1-1-tf-executed-comments": "False",
-    "q1-2-tf-starting-question": "True",
-    "q3-1-Using-Comments-in-Python": [
-        "To explain complex code",
-        "To improve code readability",
-        "To communicate assumptions or logic to future readers",
-    ],
-    "q3-2-Valid-Python-Comments": ["`# This is a comment`", "`#Calculate the area`"],
+    "q2-1-description-of-a-comment-in-python": 'Text ignored by the Python interpreter, used to explain code',
+    "q2-2-Comment-Functionality": 'Explains the purpose of the program',
+    "q1-1-tf-executed-comments": 'False',
+    "q1-2-tf-starting-question": 'True',
+    "q3-1-Using-Comments-in-Python": ['To explain complex code', 'To improve code readability', 'To communicate assumptions or logic to future readers'],
+    "q3-2-Valid-Python-Comments": ['`# This is a comment`', '`#Calculate the area`'],
 }
--- app/solutions/winter_2025/week_2/realquiz/_1_quiz_2_q.py
+++ app/solutions/winter_2025/week_2/realquiz/_1_quiz_2_q.py
@@ -1,15 +1,12 @@
 from typing import Any
 
-total_points: list[float] = [1.0, 1.0, 1.0, 1.0, 2.0, 2.0]
+total_points: float = [1.0, 1.0, 1.0, 1.0, 2.0, 2.0]
 
 solutions: dict[str, Any] = {
-    "q1-1-List-Immutability": "List",
-    "q1-2-String-Operations": "Hello5",
-    "q3-1-List-Slicing": "False",
-    "q3-2-Dictionary-Keys": "True",
-    "q2-1-Dictionary-Operations": [
-        "Adding new key-value pairs after creation",
-        "Using numbers as dictionary keys",
-    ],
-    "q2-2-**String Basics**": ["`upper()`", "`capitalize()`"],
+    "q1-1-List-Immutability": 'List',
+    "q1-2-String-Operations": 'Hello5',
+    "q3-1-List-Slicing": 'False',
+    "q3-2-Dictionary-Keys": 'True',
+    "q2-1-Dictionary-Operations": ['Adding new key-value pairs after creation', 'Using numbers as dictionary keys'],
+    "q2-2-**String Basics**": ['`upper()`', '`capitalize()`'],
 }
