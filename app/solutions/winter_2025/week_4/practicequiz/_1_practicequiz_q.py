79b0394f0f2be36fe53b5ec32a842fb9ac774b3b
Theo Beers <theo.beers@drexel.edu>
2025-02-12 20:27:49 +0000

    Update submodule and move solution files

--- app/solutions/winter_2025/week_4/practicequiz/_1_practicequiz_q.py
+++ app/solutions/winter_2025/week_4/practicequiz/_1_practicequiz_q.py
@@ -1,13 +1,17 @@
 from typing import Any
 
-total_points: float = [1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0]
+total_points: list[float] = [1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0]
 
 solutions: dict[str, Any] = {
-    "q1-1-solar-panel-monitoring": 'Optimal Solar Output',
-    "q1-2-wind-turbine-efficiency": 'The loop runs indefinitely.',
-    "q3-1-air-quality-check": 'True',
-    "q3-2-wind-speed-loop": 'True',
-    "q3-3-solar-battery-break": 'True',
-    "q2-1-battery-charge-logic": ['An `else` block is optional.', 'You can use multiple `elif` conditions to handle different voltage ranges.', 'The `if` statement evaluates conditions to determine which code block to execute.'],
-    "q2-2-hydro-turbine-loops": ['`for` loops.', '`while` loops.'],
+    "q1-1-solar-panel-monitoring": "Optimal Solar Output",
+    "q1-2-wind-turbine-efficiency": "The loop runs indefinitely.",
+    "q3-1-air-quality-check": "True",
+    "q3-2-wind-speed-loop": "True",
+    "q3-3-solar-battery-break": "True",
+    "q2-1-battery-charge-logic": [
+        "An `else` block is optional.",
+        "You can use multiple `elif` conditions to handle different voltage ranges.",
+        "The `if` statement evaluates conditions to determine which code block to execute.",
+    ],
+    "q2-2-hydro-turbine-loops": ["`for` loops.", "`while` loops."],
 }
