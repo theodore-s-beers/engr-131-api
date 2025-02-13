3ac74beaaaafafddbcc75576ed0821e0524acf2e
Theo Beers <theo.beers@drexel.edu>
2025-02-13 22:27:27 +0000

    Update submodule and move solution files

--- app/solutions/winter_2025/week_4/readings/_8_mortified_loops_q.py
+++ app/solutions/winter_2025/week_4/readings/_8_mortified_loops_q.py
@@ -1,13 +1,16 @@
 from typing import Any
 
-total_points: float = [1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0]
+total_points: list[float] = [1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0]
 
 solutions: dict[str, Any] = {
-    "q1-1-mortified-loop-syntax": '`for i in range(10):`',
-    "q1-2-mortified-break-effect": 'Terminates the loop entirely',
-    "q3-1-mortified-loop-behavior": 'False',
-    "q3-2-mortified-loop-termination": 'True',
-    "q3-3-mortified-continue-effect": 'True',
-    "q2-1-mortified-loop-utilities": ['`break`', '`continue`'],
-    "q2-2-mortified-for-loop-features": ['They can iterate over strings.', 'They can iterate over lists.'],
+    "q1-1-mortified-loop-syntax": "`for i in range(10):`",
+    "q1-2-mortified-break-effect": "Terminates the loop entirely",
+    "q3-1-mortified-loop-behavior": "False",
+    "q3-2-mortified-loop-termination": "True",
+    "q3-3-mortified-continue-effect": "True",
+    "q2-1-mortified-loop-utilities": ["`break`", "`continue`"],
+    "q2-2-mortified-for-loop-features": [
+        "They can iterate over strings.",
+        "They can iterate over lists.",
+    ],
 }
