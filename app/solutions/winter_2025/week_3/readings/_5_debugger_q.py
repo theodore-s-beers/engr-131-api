99b14e406128dcb122bd480e9ab3561fc7fad6a1
jagar2 <jca92@drexel.edu>
2025-02-19 23:01:48 -0500

    update solutions

--- app/solutions/winter_2025/week_3/readings/_5_debugger_q.py
+++ app/solutions/winter_2025/week_3/readings/_5_debugger_q.py
@@ -1,22 +1,14 @@
 from typing import Any
 
-total_points: list[float] = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0]
+total_points: float = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0]
 
 solutions: dict[str, Any] = {
-    "q1-1-Debugging-History": "A literal moth stuck in a relay",
-    "q1-2-Error-Symptoms": "Identify the symptoms and check error messages",
-    "q1-3-Debugging-Techniques": "Explaining your code, line by line, to a rubber duck or inanimate object",
-    "q3-1-Error-Messages-TF-debugger": "False",
-    "q3-2-Debugging-Mindset": "True",
-    "q3-3-Rubber-Duck-Debugging": "True",
-    "q2-1-Debugging-Tools": [
-        "Print statements",
-        "Built-in debuggers in IDEs",
-        "Carefully reading error messages",
-    ],
-    "q2-2-Debugging-Scenarios": [
-        "Code crashes due to a runtime error",
-        "Code runs but produces incorrect results",
-        "Code refuses to run due to a syntax error",
-    ],
+    "q1-1-Debugging-History": 'A literal moth stuck in a relay',
+    "q1-2-Error-Symptoms": 'Identify the symptoms and check error messages',
+    "q1-3-Debugging-Techniques": 'Explaining your code, line by line, to a rubber duck or inanimate object',
+    "q3-1-Error-Messages-TF-debugger": 'False',
+    "q3-2-Debugging-Mindset": 'True',
+    "q3-3-Rubber-Duck-Debugging": 'True',
+    "q2-1-Debugging-Tools": ['Print statements', 'Built-in debuggers in IDEs', 'Carefully reading error messages'],
+    "q2-2-Debugging-Scenarios": ['Code crashes due to a runtime error', 'Code runs but produces incorrect results', 'Code refuses to run due to a syntax error'],
 }
--- app/solutions/winter_2025/week_7/readings/_10_inheritance_q.py
+++ app/solutions/winter_2025/week_7/readings/_10_inheritance_q.py
@@ -1,21 +1,13 @@
 from typing import Any
 
-total_points: list[float] = [1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0]
+total_points: float = [1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0]
 
 solutions: dict[str, Any] = {
-    "q1-1-player-inheritance": "Inheritance allows a class to reuse attributes and methods from another class.",
-    "q1-2-quarterback-subclass": "`Patrick Mahomes 97 99`",
-    "q3-1-base-class": "True",
-    "q3-2-overriding-methods": "True",
-    "q3-3-super-method": "False",
-    "q2-1-madden-positions": [
-        "Reduces code duplication by reusing attributes.",
-        "Allows specialized player classes like `Quarterback` or `WideReceiver`.",
-        "Makes it easy to add new player positions with different attributes.",
-    ],
-    "q2-2-nfl-inheritance-methods": [
-        "`def __init__(self, name, rating):`",
-        "`super().__init__(name, rating)`",
-        "`class RunningBack(Player):`",
-    ],
+    "q1-1-player-inheritance": 'Inheritance allows a class to reuse attributes and methods from another class.',
+    "q1-2-quarterback-subclass": '`Patrick Mahomes 97 99`',
+    "q3-1-base-class": 'True',
+    "q3-2-overriding-methods": 'True',
+    "q3-3-super-method": 'False',
+    "q2-1-madden-positions": ['Reduces code duplication by reusing attributes.', 'Allows specialized player classes like `Quarterback` or `WideReceiver`.', 'Makes it easy to add new player positions with different attributes.'],
+    "q2-2-nfl-inheritance-methods": ['`def __init__(self, name, rating):`', '`super().__init__(name, rating)`', '`class RunningBack(Player):`'],
 }
