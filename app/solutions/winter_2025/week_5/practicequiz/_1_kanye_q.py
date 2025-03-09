4432673b89c0262c1682644b608af59403c7bb26
Theo Beers <theo.beers@drexel.edu>
2025-03-08 19:31:56 -0500

    Update submodule and move solution files

--- app/solutions/winter_2025/week_5/practicequiz/_1_kanye_q.py
+++ app/solutions/winter_2025/week_5/practicequiz/_1_kanye_q.py
@@ -1,13 +1,20 @@
 from typing import Any
 
-total_points: float = [1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0]
+total_points: list[float] = [1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0]
 
 solutions: dict[str, Any] = {
-    "q1-1-kanye-function-definition": 'Functions must be declared with the `def` keyword.',
-    "q1-2-args-kwargs-kanye": 'Both `*args` and `**kwargs`',
-    "q3-1-function-kanye-scope": 'False',
-    "q3-2-args-order-kanye": 'True',
-    "q3-3-optional-arguments-kanye": 'True',
-    "q2-1-return-multiple-kanye-thoughts": ['A function can return multiple values using tuples.', 'A function can return different data types at the same time.'],
-    "q2-2-kwargs-usage-kanye": ['`**kwargs` allows passing multiple keyword arguments into a function.', '`**kwargs` must always be the last argument in the function signature.', '`**kwargs` arguments are accessed as a dictionary inside the function.'],
+    "q1-1-kanye-function-definition": "Functions must be declared with the `def` keyword.",
+    "q1-2-args-kwargs-kanye": "Both `*args` and `**kwargs`",
+    "q3-1-function-kanye-scope": "False",
+    "q3-2-args-order-kanye": "True",
+    "q3-3-optional-arguments-kanye": "True",
+    "q2-1-return-multiple-kanye-thoughts": [
+        "A function can return multiple values using tuples.",
+        "A function can return different data types at the same time.",
+    ],
+    "q2-2-kwargs-usage-kanye": [
+        "`**kwargs` allows passing multiple keyword arguments into a function.",
+        "`**kwargs` must always be the last argument in the function signature.",
+        "`**kwargs` arguments are accessed as a dictionary inside the function.",
+    ],
 }
