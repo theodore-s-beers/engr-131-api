bdf4f3e323da76e69c91eac3b4dea586d70a9688
Theo Beers <theo.beers@drexel.edu>
2025-03-12 22:44:05 -0400

    Update submodule and move solution files

--- alembic/versions/6a5414c3a291_do_some_stuff.py
+++ alembic/versions/6a5414c3a291_do_some_stuff.py
@@ -5,6 +5,7 @@ Revises: 2dbb284ed385
 Create Date: 2025-03-12 22:13:49.838084
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -12,8 +13,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '6a5414c3a291'
-down_revision: Union[str, None] = '2dbb284ed385'
+revision: str = "6a5414c3a291"
+down_revision: Union[str, None] = "2dbb284ed385"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
